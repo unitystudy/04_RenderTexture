e5c71043bd0da61196fd0477f7c1bab89d22d799
imagire <imagire@gmail.com>
2019-05-24 09:50:04 +0900

    Add unity files

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(0.03f * Mathf.Cos(Time.time),0,0);
+	}
+}
